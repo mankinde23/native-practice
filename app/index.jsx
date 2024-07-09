0f0886d593d7a07fe2c00a81dbf4d2feffc2db55
mankinde23 <damilolamakinde493@gmail.com>
2024-07-09 01:38:08 +0100

    added a vh to a container

--- app/index.jsx
+++ app/index.jsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
   return (
     <SafeAreaView className=" bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
-        <View className="w-full  items-center h-full px-4">
+        <View className="w-full  items-center h-full px-4 justify-center min-h-[85vh]">
           <Image
             source={images.logo}
             className="w-[130px] h-[84px]"
